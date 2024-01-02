d2bfcd4a0754bf980918d29a54e0d20860cfc09f
Gustavo Costa Araújo <106911322+coolstavo@users.noreply.github.com>
2024-01-02 12:23:56 +0100

    Board refactoren
    - ToString in Ship
    - Board is nu abstract
    - Special board voor Zeeslag, van de instantie die extents van Board

---
+++ Zeeslag/ZeeslagBoard.java
@@ -0,0 +1,35 @@
+package Zeeslag;
+
+import Game.Board;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZeeslagBoard extends Board {
+
+    private List<Ship> placedShips;
+
+    //--------------------------------CONSTRUCTOR--------------------------------
+
+    public ZeeslagBoard(int nrOfRows, int nrOfColumns) {
+        super(nrOfRows, nrOfColumns);
+        this.placedShips = new ArrayList<>();
+    }
+
+    //-------------------------------GETTERS--------------------------------
+
+    public List<Ship> getPlacedShips() {
+        return placedShips;
+    }
+
+
+    //-------------------------------METHODS--------------------------------
+
+    public boolean isShipAlreadyPlaced(Ship ship) {
+        return placedShips.contains(ship);
+    }
+
+    public void placeShip(Ship ship) {
+        placedShips.add(ship);
+    }
+}
